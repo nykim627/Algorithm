20223a6f5ad7409a501438b3bd737828163055cf
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-01-20 21:22:18 +0900

    [level 0] Title: 특수문자 출력하기, Time: 147.97 ms, Memory: 62.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+            // 자바에서 작은 따옴표(')는 문자열 안에서 그 자체로 허용
+            // 이스케이프를 해줘도 상관은 없다.
+            // 역슬래시(\)는 항상 이스케이프 문자이므로, 문자열 내에 역슬래시 사용하고 싶다면 \\로 써줘야햠
+    }
+}
