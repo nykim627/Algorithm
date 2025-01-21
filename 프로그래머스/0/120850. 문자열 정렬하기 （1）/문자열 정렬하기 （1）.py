709470f33bf76809b5e9d4e7d6f6af49a62606fc
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-01-21 15:52:47 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -0,0 +1,7 @@
+def solution(my_string):
+    answer = []
+    for i in my_string:
+        if i in ['0','1','2','3','4','5','6','7','8','9']:
+            answer.append(int(i))
+        answer = sorted(answer)
+    return answer
