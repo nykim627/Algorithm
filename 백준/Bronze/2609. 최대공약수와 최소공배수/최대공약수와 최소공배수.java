fe02ea2b6843cf5691a372816fca37797dd9accc
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-01-31 22:09:56 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 172 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,31 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        // 문제 2609 해결을 위한 코드를 작성하세요.
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        // 최대공약수(GCD; Greatest Common Divisor)
-        // 유클리드 호제법: 이전에 나누는 수(n)와 나머지(m)가 새롭게 나눠지는 수(n)와 나누는 수(m)가 된다.
-        // m==0일 때의 n(나누는값)이 최대공약수이다.
+        // 최대공약수(gcl)
+        // 유클리드 호제법을 이용하여 구하기. 기존의 나누는 값(m)과 나머지(n%m)가 새롭게 나눠지는 값(n)과 나누는 값(m)이 된다.
+        // 나머지 m=0일 때의 나누는 값(n)이 최대공약수이다.
         int n = a;
         int m = b;
         while(m!=0){
-            int tmp = m;
-            m = n%m;
-            n = tmp;  //최ㅐ공약수
+            int tmp = m; //기존의 나누는 값을 임시 변수에 저장해둠 -> 새롭게 나누지는 값(n)이 되야 한다.
+            m = n%m;     //기존의 나머지(n%m) -> 새롭게 나누는 값(m)이 되야 한다.
+            n = tmp;
         }
-        int gcd = n;
+        int gcl = n;
 
-        // 최소공배수(LCM; Least Common Multiple)
-        // 최소공배수는 두 값 a, b를 곱한 값에서 최대공약수를 나눈 값! 즉, a*b/n이다.
-        int lcm = a*b/gcd;
+        // 최소공배수(lcm)
+        // 두 값 a와 b의 곱에서 최대공약수로 나눈 값이 최소공배수이다.
+        int lcm = a*b/gcl;
 
-        System.out.println(gcd);
+        System.out.println(gcl);
         System.out.println(lcm);
+
 
     }
 }
