6682d29a36716ef01b1d03e862710c1405c6f322
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-01-31 21:46:01 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 184 ms, Memory: 18404 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,28 +1,30 @@
+// B10809.java
+// 문제 10809: 알파벳 찾기 - 각 알파벳의 첫 등장 위치를 출력.
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        // 문제 10809 해결을 위한 코드를 작성하세요.
         Scanner sc = new Scanner(System.in);
 
         String my_string = sc.next();
+        int[] arr = new int[26];
+        Arrays.fill(arr,-1); //기본값을 -1로 초기화
 
-        int[] answer = new int[26];
-        Arrays.fill(answer, -1); // 기본값 -1로 초기화
-
-        for (int j = 0; j < my_string.length(); j++) {
-            char ch = my_string.charAt(j);
-            int index = ch - 'a'; // 'a'의 ASCII 값(97)부터 0~25로 변환
-
-            if (answer[index] == -1) { // 첫 등장 위치만 저장
-                answer[index] = j;
+        // 각각의 알파벳이 처음 등장하는 위치가 있으면 해당 인덱스는 배열에 넣은 후 내부 for문을 빠져나온다.
+        for(int i=0;i<my_string.length();i++){
+            for(int j=0;j<arr.length;j++){
+                if(my_string.charAt(i)==j+97&&arr[j]==-1){
+                    arr[j]=i;
+                    break;
+                }
             }
         }
-        //System.out.println(Arrays.toString(answer));
-
-        for(int i=0;i<answer.length;i++){
-            System.out.print(answer[i]+" ");
+        
+        // 배열 출력하기
+        for(int j=0;j<arr.length;j++){
+            System.out.print(arr[j]+" ");
         }
     }
 }
