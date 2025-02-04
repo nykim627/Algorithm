b14005f6b4a58a4bbaa3b84b04217c88c48d0608
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-02-04 16:07:10 +0900

    [Bronze II] Title: Hashing, Time: 184 ms, Memory: 17708 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,30 +1,34 @@
 import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         int l = sc.nextInt();
         String str = sc.next();
-        int sum = 0;
-        for(int i=0;i<l;i++) {
-        	sum += ((str.charAt(i)-96)*square(31,i));
+        long sum = 0;
+        long m = 1234567891;
+        int r = 31;
+        
+        for (int i = 0; i < l; i++) {
+            long value = (str.charAt(i) - 'a' + 1);
+            sum = (sum + (value * modPow(r, i, m)) % m) % m;  // 모듈러 연산 적용
         }
-        
-        int m = 1234567891;
-        int h = sum%m;
-        
-		System.out.println(h);
-		sc.close();
-	}
-	
-	public static int square(int r, int i) {
-		int mul = 1;
-		for(int j=1;j<=i;j++) {
-			mul *= r;
-		}
-		return mul;
-	}
 
+        System.out.println(sum);
+        sc.close();
+    }
+
+    // 빠른 거듭제곱 (Exponentiation by Squaring)
+    public static long modPow(long base, int exp, long mod) {
+        long result = 1;
+        while (exp > 0) {
+            if (exp % 2 == 1) {  // 홀수이면 base를 한 번 곱함
+                result = (result * base) % mod;
+            }
+            base = (base * base) % mod;  // base를 제곱하여 지수를 줄임
+            exp /= 2;
+        }
+        return result;
+    }
 }
