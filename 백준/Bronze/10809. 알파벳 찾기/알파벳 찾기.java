6eae42d3d60b27bd30b449ca201a3f267e27ca2c
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-02-09 16:21:17 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 184 ms, Memory: 18280 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,6 +1,5 @@
 // B10809.java
 // 문제 10809: 알파벳 찾기 - 각 알파벳의 첫 등장 위치를 출력.
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -10,18 +9,21 @@ public class Main {
 
         String my_string = sc.next();
         int[] arr = new int[26];
-        Arrays.fill(arr,-1); //기본값을 -1로 초기화
 
-        // 각각의 알파벳이 처음 등장하는 위치가 있으면 해당 인덱스는 배열에 넣은 후 내부 for문을 빠져나온다.
-        for(int i=0;i<my_string.length();i++){
-            for(int j=0;j<arr.length;j++){
-                if(my_string.charAt(i)==j+97&&arr[j]==-1){
-                    arr[j]=i;
-                    break;
-                }
-            }
+        // 알파벳 배열 순회하면서 각각의 알파벳이 처음 등장하는 위치가 있으면 해당 인덱스는 배열에 넣은 후 내부 for문을 빠져나온다.
+        for(int i=0;i<arr.length;i++){
+//            for(int j=0;j<my_string.length();j++){
+//                if(my_string.charAt(j)-'a'==i){ //'a'를 빼면 항상 97을 뺀 효과가 있다.
+//                    arr[i]=j;
+//                    break;
+//                }else{
+//                    arr[i]=-1;
+//                }
+//            }
+            char alphabet = (char)(i+'a');
+            arr[i] = my_string.indexOf(String.valueOf(alphabet)); // 해당 값을 문자열의 처음부터 찾기 시작해서 있으면 그값의 인덱스 반환, 없으면 -1 반환.
         }
-        
+
         // 배열 출력하기
         for(int j=0;j<arr.length;j++){
             System.out.print(arr[j]+" ");
