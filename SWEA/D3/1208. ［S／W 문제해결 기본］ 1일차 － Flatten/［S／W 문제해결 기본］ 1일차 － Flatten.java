d2109b874920bf3135613d8271783bedb1e8a81e
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-02-10 14:49:24 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 176 ms, Memory: 31,796 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
-		// 최빈값 찾기
+		// Flatten
 		Scanner sc = new Scanner(System.in);
 
 		int T = 10; // tc수
@@ -11,43 +11,42 @@ public class Solution {
 		for (int tc = 1; tc <= T; tc++) {
 
 			int dumpTime = sc.nextInt(); // 덤프 횟수 (1이상 1000이하)
+			int[] arr = new int[100]; // 상자 높이 배열 (가로 길이는 항상 100)
 
-			int[] arr = new int[100]; // 상자 높이 배열 (가로길이는 항상 100)
-
-			int max=0, min=100;
-			int maxIdx = -1;
-			int minIdx = -1;
-			int diff=0;
+            for (int i = 0; i < 100; i++) {
+                arr[i] = sc.nextInt(); // 배열값 입력
+            }
 			
-			for (int i = 0; i < arr.length; i++) {
-				arr[i] = sc.nextInt(); // 배열값 입력
+			//처음부터 각 높이별 상자 개수를 기록해둔다.
+			int[] count = new int[101]; //1~100 높이 개수 저장
+			
+			//각 높이를 갖는 가로칸의 개수를 세서 카운팅 배열에 저장
+			for(int h: arr) {
+				count[h]++;
 			}
 			
-			for(int dt=1;dt<=dumpTime;dt++) {	
-				for (int i = 0; i < arr.length; i++) {
-					if(max<arr[i]) { //최댓값 찾기
-						max = arr[i]; maxIdx = i;
-					}
-					else if(min>arr[i]) { //최솟값 찾기
-						min = arr[i]; minIdx=i;
-					}
-				}
-				
-				arr[maxIdx]-=1; max=0; //최댓값 갱신 필요
-				arr[minIdx]+=1; min=100; //최솟값 갱신 필요
-				
-				for (int i = 0; i < arr.length; i++) {
-					if(max<arr[i]) { //최댓값 다시 찾기
-						max = arr[i]; maxIdx = i;
-					}
-					else if(min>arr[i]) { //최솟값 다시 찾기
-						min = arr[i]; minIdx=i;
-					}
-				}
-				diff = arr[maxIdx]-arr[minIdx]; //다시 찾은 최고점과 최저점의 높이 차
-			}//dt for문 끝
 			
-			System.out.println("#" + tc + " " + diff); //결과 출력
+			//최고점(start)과 최저점(end) 위치를 따로 추적하며 평탄화 진행(O(dump))
+			int start=1, end=100; 
+			
+			while(dumpTime>0&&start<end) {
+				//최저점 찾기(현재 높이에 상자가 없으면 다음 높이로)
+				while(count[start]==0) start++;
+				//최고점 찾기
+				while(count[end]==0) end--;
+				
+				//평탄화 수행
+				count[start]--; //최저점에서 하나 빼기(가장 낮은 상자 높이를 갖는 칸이 하나 줄어듦)
+				count[start+1]++; //최저점+1에 추가
+				count[end]--; //최고점에서 하나 빼기
+				count[end-1]++; //최고점-1에 추가
+				dumpTime--; //덤프 횟수 감소
+			}//dumpTime while문 끝
+			
+			while(count[start]==0) start++;
+			while(count[end]==0) end--;
+			
+			System.out.println("#" + tc + " " + (end-start)); //결과 출력
 		}//tc for문 끝
 		sc.close(); //스캐너 닫기
 	}//main끝
