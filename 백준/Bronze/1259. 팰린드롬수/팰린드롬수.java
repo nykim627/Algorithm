d343c7dcd0ef293b9f975be0135f63d0e0a271cd
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-02-11 12:52:14 +0900

    [Bronze I] Title: 팰린드롬수, Time: 160 ms, Memory: 17492 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -1,28 +1,30 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+		while (true) { // 무한반복
+			String str = sc.next(); // 문자열 입력
+			if (str.equals("0"))
+				break; // (무한반복종료조건) 0 입력받으면 반복문 break;
+			else {
+				int len = str.length(); // 문자열 길이
+				boolean isPal = true; // 팰린드롬수인지 저장하는 변수
 
-        int k = 1;
-        while(k!=0){
-            String n = sc.next();
-            if(n.equals("0")){
-                k=0;
-            }else{
-                String s2 = "";
-
-                for(int i=0;i<n.length();i++){
-                    s2 += n.charAt((n.length()-1)-i);
-                }
-//                System.out.println(n);
-//                System.out.println(s2);
-
-                System.out.println(n.equals(s2) ? "yes" : "no");
-            }
-
-        }
-
-    }
+				for (int i = 0; i < len / 2; i++) {
+					if (str.charAt(i) != str.charAt(len - 1 - i)) {
+						isPal = false;
+						break;
+					}
+				}
+				if (isPal) {
+					System.out.println("yes");
+				} else {
+					System.out.println("no");
+				}
+			}
+		}
+	}
 }
