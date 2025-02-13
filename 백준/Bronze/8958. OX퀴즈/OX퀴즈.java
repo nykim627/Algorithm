32f1049d662e6d26b4c43263271bf34145d2957e
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-02-13 21:39:54 +0900

    [Bronze II] Title: OX퀴즈, Time: 188 ms, Memory: 17844 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -1,24 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        for(int i=0;i<t;i++) {
-            String q = sc.next();
-            int sum = 0;
-            int tt = 0;
-            for (int j = 0; j < q.length(); j++) {
-                if (q.charAt(j) == 'O') {
-                    sum += 1;
-                } else if (q.charAt(j) == 'X') {
-                    sum = 0;
-                }
-                tt+=sum;
-            }
-            System.out.println(tt);
-        }
-    }
+        //OX퀴즈
 
+        int t = sc.nextInt(); //tc 개수
+
+        for(int tc=0;tc<t;tc++){
+            String str = sc.next(); //퀴즈 결과 문자열
+
+            int sum = 0; // 총점수
+            int subsum = 0; //부분합(연속 O 점수)
+            for(int i=0;i<str.length();i++){
+                if(str.charAt(i)=='O'){
+                    subsum++;
+                }else{
+                    subsum = 0; // X 만나면 부분합 초기화
+                }
+                sum += subsum;
+            }//for문 끝
+            System.out.println(sum);
+        }//tc순회 끝
+
+
+
+        sc.close();
+    }//main끝
 }
