d44cc7a2ac89614aa8989cbafb20c5320299a535
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-02-19 23:22:06 +0900

    [Bronze I] Title: 세로읽기, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,36 +1,31 @@
-//package Week3.problem;
-
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //세로읽기
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[][] arr = new String[5][15]; //최대 15개의 글자이므로 최대길이로 설정
+        char[][] arr = new char[5][15]; // 기본값 '\0' (null character)로 초기화됨
 
-        //입력
-        for(int i=0;i<5;i++){
-            String tmp = sc.next(); //문자열 입력받음
-            for(int j=0;j<tmp.length();j++){ //입력받은 문자열의 문자를 하나씩 집어넣음
-                arr[i][j] = Character.toString(tmp.charAt(j)); //
+        // 입력 처리
+        for (int i = 0; i < 5; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < line.length(); j++) {
+                arr[i][j] = line.charAt(j);
             }
         }
 
-        for(int c=0;c<15;c++){ //열고정 
-            for(int r=0;r<5;r++){ //행순회
-                if(arr[r][c]!=null){ //값이 기본값인 null이면 입력되지 않은 것이므로 출력하지 않음
-                    System.out.print(arr[r][c]);
+        // 출력 처리
+        StringBuilder sb = new StringBuilder();
+        for (int c = 0; c < 15; c++) { 
+            for (int r = 0; r < 5; r++) { 
+                if (arr[r][c] != '\0') {  // '\0'이면 건너뜀
+                    sb.append(arr[r][c]);
                 }
             }
         }
 
-
-
-
+        System.out.println(sb.toString());
     }
 }
