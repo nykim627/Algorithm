c99c530c9f017170aea0ab3cc7bc072982b36406
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-03-13 16:00:13 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 452 ms, Memory: 25408 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -1,61 +1,42 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
-//B9184 신나는 함수 실행
-//시간:
-//메모리:
-//아이디어: 재귀 활용, memoization
+import java.util.*;
+import java.io.*;
+
 public class Main {
-	static int a;
-	static int b;
-	static int c;
-	static int[][][] memo;
+    static int[][][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // 011 001 010 000
+        dp = new int[21][21][21];
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        while (true) {
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-		memo = new int[21][21][21];
-		StringTokenizer st;
-		while (true) {
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken()); // -50 ≤ a, b, c ≤ 50
-			b = Integer.parseInt(st.nextToken());
-			c = Integer.parseInt(st.nextToken());
-			if (a == -1 && b == -1 && c == -1) {
-				break;
-			} else {
-				bw.write(String.format("w(%d, %d, %d) = ", a, b, c) + recur(a, b, c) + "\n");
-			}
-		}
+            if (a == -1 && b == -1 && c == -1) {
+                break;
+            }
 
-		bw.flush();
-		bw.close();
-		br.close();
+            System.out.println("w(" + a + ", " + b + ", " + c + ") = " + w(a, b, c));
+            st = new StringTokenizer(br.readLine());
+        }
+    }
 
-	}// main
-
-	static int recur(int a, int b, int c) {
-		if (a <= 0 || b <= 0 || c <= 0)
-			return 1;
-		if (a > 20 || b > 20 || c > 20)
-			return recur(20, 20, 20);
-		if (memo[a][b][c] >= 1)
-			return memo[a][b][c];
-		if (a < b && b < c) {
-			memo[a][b][c - 1] = recur(a, b, c - 1);
-			memo[a][b - 1][c - 1] = recur(a, b - 1, c - 1);
-			memo[a][b - 1][c] = recur(a, b - 1, c);
-			return memo[a][b][c - 1] + memo[a][b - 1][c - 1] - memo[a][b - 1][c];
-		}
-		memo[a - 1][b][c] = recur(a - 1, b, c);
-		memo[a - 1][b - 1][c] = recur(a - 1, b - 1, c);
-		memo[a - 1][b][c - 1] = recur(a - 1, b, c - 1);
-		memo[a - 1][b - 1][c - 1] = recur(a - 1, b - 1, c - 1);
-		return memo[a - 1][b][c] + memo[a - 1][b - 1][c] + memo[a - 1][b][c - 1] - memo[a - 1][b - 1][c - 1];
-	}// recur
+    static int w(int a, int b, int c) {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return 1;
+        }
+        if (a > 20 || b > 20 || c > 20) {
+            return w(20, 20, 20);
+        }
+        if (dp[a][b][c] != 0) {
+            return dp[a][b][c];
+        }
+        if (a < b && b < c) {
+            return dp[a][b][c] = w(a,b,c-1)+w(a,b-1,c-1)-w(a,b-1,c);
+        }
+        return dp[a][b][c]=w(a-1,b,c)+w(a-1,b-1,c)+w(a-1,b,c-1)-w(a-1,b-1,c-1);
+    }
 }
