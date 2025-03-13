a27c9750a959ce78216a72c48af86a15574e2ec5
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-03-13 15:42:47 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 1168 ms, Memory: 80044 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -14,46 +14,48 @@ public class Main {
 	static int b;
 	static int c;
 	static int[][][] memo;
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		memo = new int[21][21][21];
 		StringTokenizer st;
-		while(true) {
+		while (true) {
 			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken()); //-50 ≤ a, b, c ≤ 50
+			a = Integer.parseInt(st.nextToken()); // -50 ≤ a, b, c ≤ 50
 			b = Integer.parseInt(st.nextToken());
 			c = Integer.parseInt(st.nextToken());
-			if(a==-1&&b==-1&&c==-1) {
+			if (a == -1 && b == -1 && c == -1) {
 				break;
-			}else {
-				bw.write(String.format("w(%d, %d, %d) = ", a,b,c)+recur(a,b,c)+"\n");
+			} else {
+				bw.write(String.format("w(%d, %d, %d) = ", a, b, c) + recur(a, b, c) + "\n");
 			}
 		}
-		
+
 		bw.flush();
 		bw.close();
 		br.close();
-		
-	}//main
-	
+
+	}// main
+
 	static int recur(int a, int b, int c) {
-		if(a<=0||b<=0||c<=0) return 1;
-		else if(a>20||b>20||c>20) return recur(20,20,20);
-		else if(memo[a][b][c]>=1) return memo[a][b][c];
-		else if(a<b&&b<c) {
-			memo[a][b][c-1] = recur(a,b,c-1);
-			memo[a][b-1][c-1] = recur(a,b-1,c-1);
-			memo[a][b-1][c] = recur(a,b-1,c);
-			return memo[a][b][c-1]+memo[a][b-1][c-1]-memo[a][b-1][c];
+		if (a <= 0 || b <= 0 || c <= 0)
+			return 1;
+		if (a > 20 || b > 20 || c > 20)
+			return recur(20, 20, 20);
+		if (memo[a][b][c] >= 1)
+			return memo[a][b][c];
+		if (a < b && b < c) {
+			memo[a][b][c - 1] = recur(a, b, c - 1);
+			memo[a][b - 1][c - 1] = recur(a, b - 1, c - 1);
+			memo[a][b - 1][c] = recur(a, b - 1, c);
+			return memo[a][b][c - 1] + memo[a][b - 1][c - 1] - memo[a][b - 1][c];
 		}
-		else {
-			memo[a-1][b][c] = recur(a-1,b,c);
-			memo[a-1][b-1][c] = recur(a-1,b-1,c);
-			memo[a-1][b][c-1] = recur(a-1,b,c-1);
-			memo[a-1][b-1][c-1] = recur(a-1,b-1,c-1);
-			return memo[a-1][b][c]+memo[a-1][b-1][c]+memo[a-1][b][c-1]-memo[a-1][b-1][c-1];
-		}
-	}//recur
+		memo[a - 1][b][c] = recur(a - 1, b, c);
+		memo[a - 1][b - 1][c] = recur(a - 1, b - 1, c);
+		memo[a - 1][b][c - 1] = recur(a - 1, b, c - 1);
+		memo[a - 1][b - 1][c - 1] = recur(a - 1, b - 1, c - 1);
+		return memo[a - 1][b][c] + memo[a - 1][b - 1][c] + memo[a - 1][b][c - 1] - memo[a - 1][b - 1][c - 1];
+	}// recur
 }
