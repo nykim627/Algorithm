1404e151c5756037969272230b0708f4c1e5f38a
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-05-11 00:07:50 +0900

    [Bronze V] Title: 시험 성적, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,19 +1,20 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-        if(a>=90 && a<=100){
-            System.out.println("A");
-        }else if(a>=80){
-            System.out.println("B");
-        }else if(a>=70){
-            System.out.println("C");
-        }else if(a>=60){
-            System.out.println("D");
-        }else{
-            System.out.println("F");
-        }
-    }
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		
+		if(a>=90 && a<=100) System.out.println("A");
+		else if(a>=80) System.out.println("B");
+		else if(a>=70) System.out.println("C");
+		else if(a>=60) System.out.println("D");
+		else System.out.println("F");
+	}
+
 }
