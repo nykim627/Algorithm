f394e016574c99772f6dabbd684fd002821eaea4
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-05-10 23:52:21 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -1,9 +1,17 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        System.out.println(a-543);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    }
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int year = Integer.parseInt(br.readLine());
+		System.out.println(year-543);
+		
+		
+		br.close();
+	}
+
 }
