3326b235644c79b75d190f425f6b645fa7434d1e
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-05-11 00:03:16 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 14040 KB -BaekjoonHub

--- 백준/Bronze/10172. 개/개.java
+++ 백준/Bronze/10172. 개/개.java
@@ -1,9 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("|\\_/|");
-        System.out.println("|q p|   /}");
-        System.out.println("( 0 )\"\"\"\\");
-        System.out.println("|\"^\"`    |");
-        System.out.println("||_/=\\\\__|");
-    }
+	public static void main(String[] args) throws IOException {
+		
+		System.out.println("|\\_/|\n"
+				+ "|q p|   /}\n"
+				+ "( 0 )\"\"\"\\\n"
+				+ "|\"^\"`    |\n"
+				+ "||_/=\\\\__|");
+	}
+
 }
