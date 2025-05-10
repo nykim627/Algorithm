eb71a6ffa3b7cd4c2b4578fa3e51b323d166b3d1
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-05-11 00:12:58 +0900

    [Bronze V] Title: 사분면 고르기, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -1,18 +1,19 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-        if(a>0 && b>0){
-            System.out.println(1);
-        }else if(a<0 && b>0){
-            System.out.println(2);
-        }else if(a<0 && b<0){
-            System.out.println(3);
-        }else{
-            System.out.println(4);
-        }
-    }
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int x = Integer.parseInt(br.readLine());
+		int y = Integer.parseInt(br.readLine());
+		
+		if(x>0&&y>0) System.out.println(1);
+		else if(x<0&&y>0) System.out.println(2);
+		else if(x<0&&y<0) System.out.println(3);
+		else System.out.println(4);
+		
+	}
 }
