84ff20e3560061d6fe543f0694e20cda6d913f97
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-05-11 00:02:38 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 14000 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,8 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("\\    /\\");
-        System.out.println(" )  ( ')");
-        System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
-    }
+	public static void main(String[] args) throws IOException {
+		
+		System.out.println("\\    /\\\n"
+				+ " )  ( ')\n"
+				+ "(  /  )\n"
+				+ " \\(__)|");
+	}
+
 }
