cf379b4b8293a4c73bc85ef348527346441fea89
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-05-11 00:10:18 +0900

    [Bronze V] Title: 윤년, Time: 108 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,13 +1,16 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-        if((a%4==0 && a%100!=0)||a%400==0){
-            System.out.println(1);
-        }else{
-            System.out.println(0);
-        }
-    }
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		
+		if((a%4==0 && a%100!=0) || a%400==0) System.out.println(1);
+		else System.out.println(0);
+	}
 }
