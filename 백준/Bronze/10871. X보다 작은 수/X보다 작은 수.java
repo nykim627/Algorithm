d217e20f383020b0273442f61ed80b740ab58ca7
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-05-11 23:21:53 +0900

    [Bronze V] Title: X보다 작은 수, Time: 140 ms, Memory: 15324 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(st.nextToken());
+		int x = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			if (arr[i] < x) {
+				sb.append(arr[i]).append(" ");
+			}
+		}
+		
+		System.out.println(sb.toString());
+
+		br.close();
+
+	}
+}
