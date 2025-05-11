fb8a40a10d6388c4ddf11e0b483bc7eaa660b606
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-05-12 00:06:10 +0900

    [Bronze I] Title: 평균, Time: 108 ms, Memory: 14464 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,26 +1,34 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        // 문제 1546 해결을 위한 코드를 작성하세요.
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();  //과목 개수 n
-        int[] arr = new int[n];  //개별 점수 저장하는 배열
-        int max = 0;
-        for(int i=0;i<n;i++){
-            arr[i] = sc.nextInt();  //개별 점수
-            if(arr[i]>max){
-                max = arr[i];
-            }
-        }
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+		int max = Integer.MIN_VALUE;
+		for(int i=0;i<n;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			max = Math.max(max, arr[i]);
+		}
+		double[] newArr = new double[n];
+		double sum = 0;
+		for(int i=0;i<n;i++) {
+			newArr[i] = ((double)arr[i])/max * 100;
+			sum += newArr[i];
+		}
+		
+		System.out.println(sum/n);
 
-        double[] arr2 = new double[n];  //새로운 점수 저장하는 배열
-        double sum = 0;
-        for(int i=0;i<n;i++){
-            arr2[i] = (double) arr[i]/max * 100;
-            sum+=arr2[i];
-        }
-        System.out.println(sum/n);
-    }
+		br.close();
+
+	}
 }
