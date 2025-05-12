f7601548830a59e4f4c72996162212c01826d63b
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-05-13 00:47:07 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 116 ms, Memory: 15880 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,32 +1,29 @@
-// B10809.java
-// 문제 10809: 알파벳 찾기 - 각 알파벳의 첫 등장 위치를 출력.
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        // 문제 10809 해결을 위한 코드를 작성하세요.
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String my_string = sc.next();
-        int[] arr = new int[26];
+		String s = br.readLine();
+		int[] arr = new int[26];
+		Arrays.fill(arr, -1);
+		for(int i=0;i<s.length();i++) {
+			int idx = s.charAt(i)-'a';
+			if(arr[idx]==-1) arr[idx] = i;
+		}
+		
+		for(int i=0;i<26;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+		br.close();
 
-        // 알파벳 배열 순회하면서 각각의 알파벳이 처음 등장하는 위치가 있으면 해당 인덱스는 배열에 넣은 후 내부 for문을 빠져나온다.
-        for(int i=0;i<arr.length;i++){
-//            for(int j=0;j<my_string.length();j++){
-//                if(my_string.charAt(j)-'a'==i){ //'a'를 빼면 항상 97을 뺀 효과가 있다.
-//                    arr[i]=j;
-//                    break;
-//                }else{
-//                    arr[i]=-1;
-//                }
-//            }
-            char alphabet = (char)(i+'a');
-            arr[i] = my_string.indexOf(String.valueOf(alphabet)); // 해당 값을 문자열의 처음부터 찾기 시작해서 있으면 그값의 인덱스 반환, 없으면 -1 반환.
-        }
-
-        // 배열 출력하기
-        for(int j=0;j<arr.length;j++){
-            System.out.print(arr[j]+" ");
-        }
-    }
+	}
 }
