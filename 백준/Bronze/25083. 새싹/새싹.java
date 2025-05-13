b082bf84d99a684993973df8804c47e0460ef6ec
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-05-13 23:41:01 +0900

    [Bronze V] Title: 새싹, Time: 104 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("         ,r'\"7\n"
+				+ "r`-_   ,'  ,/\n"
+				+ " \\. \". L_r'\n"
+				+ "   `~\\/\n"
+				+ "      |\n"
+				+ "      |\n"
+				+ "");
+
+	}
+}
