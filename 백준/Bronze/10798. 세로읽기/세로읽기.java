56c0e6d9865a9d73814b89c7c8c964b49e47b397
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-05-14 01:17:51 +0900

    [Bronze I] Title: 세로읽기, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,31 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        char[][] arr = new char[5][15]; // 기본값 '\0' (null character)로 초기화됨
+		String[][] arr = new String[5][15];
 
-        // 입력 처리
-        for (int i = 0; i < 5; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < line.length(); j++) {
-                arr[i][j] = line.charAt(j);
-            }
-        }
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<5;i++) {
+			String input = br.readLine();
+			for(int j=0;j<input.length();j++) {
+				arr[i][j] = Character.toString(input.charAt(j));
+			}
+		}
+		
+		for(int j=0;j<15;j++) {
+			for(int i=0;i<5;i++) {
+				if(arr[i][j]!=null) sb.append(arr[i][j]);
+			}
+		}
+		
+		System.out.println(sb.toString());
+		br.close();
 
-        // 출력 처리
-        StringBuilder sb = new StringBuilder();
-        for (int c = 0; c < 15; c++) { 
-            for (int r = 0; r < 5; r++) { 
-                if (arr[r][c] != '\0') {  // '\0'이면 건너뜀
-                    sb.append(arr[r][c]);
-                }
-            }
-        }
-
-        System.out.println(sb.toString());
-    }
+	}
 }
