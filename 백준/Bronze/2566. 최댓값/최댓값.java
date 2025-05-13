4989c679805c7972d68c596ef4692431831f5602
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-05-14 01:12:05 +0900

    [Bronze III] Title: 최댓값, Time: 132 ms, Memory: 15920 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,33 +1,31 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        //최대값
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[][] arr = new int[9][9]; //9x9 배열 (0행, 0열부터 시작)
-        int max = 0; //최대값
-        int maxcol = 1; //모든 값이 0일 때, maxcol, maxrow 1로 초기화해둬야지 1행1열이 나온다(주어지는 수는 100보다 작은 자연수 또는 0이기 때문)
-        int maxrow = 1;
-        for(int r=0;r<9;r++){
-            for(int c=0;c<9;c++){
-                arr[r][c] = sc.nextInt();  //배열값 입력
+		int[][] arr = new int[9][9];
+		int max = Integer.MIN_VALUE;
+		int row=-1; 
+		int col=-1;
+		for(int i=0;i<9;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j=0;j<9;j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				if(max<arr[i][j]) {
+					max = arr[i][j];
+					row = i;
+					col = j;
+				}
+			}
+		}
+		
+		System.out.println(max);
+		System.out.println((row+1)+" "+(col+1));
+		br.close();
 
-                //입력받으면서 최대값 및 그때의 인덱스 바로바로 갱신
-                if(arr[r][c]>max){
-                    max = arr[r][c];
-                    maxrow = r+1; //0행부터 시작이므로 문제에 맞게 +1해줘야 함
-                    maxcol = c+1; //0열부터 시작이므로 문제에 맞게 +1해줘야 함
-                }
-            }
-
-        }
-
-        System.out.println(max);
-        System.out.println(maxrow+" "+maxcol);
-
-        sc.close();
-    }//main끝
+	}
 }
