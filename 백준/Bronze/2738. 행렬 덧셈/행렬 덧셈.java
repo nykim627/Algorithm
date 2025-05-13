5e8a92ecc2c424b21d6b9dce6f09c25b3e96537b
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-05-14 01:08:24 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 296 ms, Memory: 19100 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,36 +1,40 @@
-
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        //행렬덧셈
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt(); //행개수
-        int m = sc.nextInt(); //열개수
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[][] arr1 = new int[n][m];
+		int[][] arr2 = new int[n][m];
+		
+		for(int i=0;i<n;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0;j<m;j++) {
+				arr1[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for(int i=0;i<n;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0;j<m;j++) {
+				arr2[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<m;j++) {
+				System.out.print((arr1[i][j]+arr2[i][j])+" ");
+			}
+			System.out.println();
+		}
+		
+		br.close();
 
-        int[][] arr1 = new int[n][m];
-        int[][] arr2 = new int[n][m];
-
-        //arr1 값입력
-        for(int r=0;r<n;r++){
-            for(int c=0;c<m;c++){
-                arr1[r][c] = sc.nextInt();
-            }
-        }
-        //arr2 값입력
-        for(int r=0;r<n;r++){
-            for(int c=0;c<m;c++){
-                arr2[r][c] = sc.nextInt();
-            }
-        }
-        //행렬덧셈
-        for(int r=0;r<n;r++) {
-            for (int c = 0; c < m; c++) {
-                System.out.print((arr1[r][c] + arr2[r][c]) + " ");
-            }
-            System.out.println();
-        }
-    }
+	}
 }
