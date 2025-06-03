ef84a8dc90272519b79a2385bf038586b0c7fafd
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-06-03 19:24:54 +0900

    [Bronze II] Title: 벌집, Time: 100 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,29 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		
-		int n = sc.nextInt();
-		
-		int m = 2;
-		int answer = 0;
-		while(m!=0) {
-			if(n==1) {
-				answer = 1;
-                m=0;
-			}else {
-				// 등차수열의 합 = ((첫째항+마지막항)*(항개수))/2
-				int sum_m = 3*(m-1)*(m-1)+3*(m-1)+1;   //m층까지의 등차수열의 합
-				int sum_m_1 = 3*(m-2)*(m-2)+3*(m-2)+1;  //m-1층까지의 등차수열의 합
-				if(sum_m>=n && sum_m_1<n) {
-					answer = m;
-					m=0;
-				}else {
-					m++;
-				}
-			}
+		int n = 0;
+		while(N>3*n*(n+1)+1) {
+			n++;
 		}
-		System.out.println(answer);
+		
+		System.out.println(n+1);
+		
 	}
 }
+
