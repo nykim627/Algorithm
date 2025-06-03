71abb5998f57493fa69a98d0a2ce9a1226f28632
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-06-03 21:13:38 +0900

    [Bronze II] Title: 소수 찾기, Time: 104 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,35 +1,35 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        // 문제 1978 해결을 위한 코드를 작성하세요.
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int cnt = 0; //소수 개수
-
-        for(int i=0;i<n;i++){
-            int a = sc.nextInt();
-
-            //소수 여부 파악을 위한 코드
-            int m=2;    // 나누는 값을 2부터 시작
-            while(m!=0){
-                if(a!=1){   // 1이 아닐 때만 고려
-                    if(a%m==0){  // 나머지가 0이고
-                        if(a/m==1){ // 몫이 1이라면 더이상 나누어질 수 없는 수이므로 소수이다.
-                            cnt++;
-                            m=0;
-                        }else{  // 몫이 1이 아니라면 더 나누어질 수 있는 수이므로 소수가 아니다.
-                            m=0;
-                        }
-                    }else{
-                        m++;
-                    }
-                }else{  //주어진 수가 1이면 m=0을 할당해 반복문을 멈춘다.
-                    m=0;
-                }
-            }
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int T = Integer.parseInt(br.readLine());
+        int num = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        for(int tc=0;tc<T;tc++) {
+        	int N = Integer.parseInt(st.nextToken());
+        	boolean isOk = true;
+        	if(N==1) { //1은 소수아님 
+        		isOk = false;
+        	}else {
+            	for(int i=2;i<N;i++) {
+            		if(N%i==0) {
+            			isOk = false;
+            			break;
+            		}
+            	}
+        	}
+        	//소수인 애들만 세기
+        	if(isOk) {
+        		num++;
+        	}
         }
-        System.out.println(cnt);
+        
+        System.out.println(num);
+
+
     }
 }
