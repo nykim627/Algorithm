551d6fa87078818f6cb79f1f2ab22988861c17cd
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-06-03 23:23:18 +0900

    [Bronze II] Title: 블랙잭, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,41 +1,30 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		
-		int[] arr = new int[n];
-		for(int i=0;i<n;i++) {
-			arr[i] = sc.nextInt();
-		}
-		
-		int min = Integer.MAX_VALUE;
-		int final_sum = 0;
-		int final_i = 0;
-		int final_j = 0;
-		int final_k = 0;
-		for(int i=0;i<n;i++) {
-			for(int j=i+1;j<n;j++) {
-				for(int k=j+1;k<n;k++) {
-					int sum = arr[i]+arr[j]+arr[k];
-					if(sum<=m) {
-						if(m-sum<min) {
-							min = m-sum;
-							final_sum = sum;
-//							final_i = arr[i];
-//							final_j = arr[j];
-//							final_k = arr[k];
-						}
-					}
-				}
-			}
-		}
-		System.out.println(final_sum);
-//		System.out.println(final_i);
-//		System.out.println(final_j);
-//		System.out.println(final_k);
-	}
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++) {
+        	arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<N-2;i++) {
+        	for(int j=i+1;j<N-1;j++) {
+        		for(int k=j+1;k<N;k++) {
+        			if(arr[i]+arr[j]+arr[k]<=M) {
+        				max = Math.max(max, arr[i]+arr[j]+arr[k]);
+        			}
+        		}
+        	}
+        }
+        System.out.println(max);
+    }
 }
