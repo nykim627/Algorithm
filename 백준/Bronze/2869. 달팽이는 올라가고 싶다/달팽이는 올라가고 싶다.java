c3154be763fe853817a577bc5dfc3961569f5227
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-06-03 19:45:16 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -1,12 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int v = sc.nextInt();
-        System.out.println((v-a)%(a-b)==0 ? (v-a)/(a-b)+1 : (v-a)/(a-b)+2);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());  //낮에올라감
+		int B = Integer.parseInt(st.nextToken());  //밤에내려감
+		int V = Integer.parseInt(st.nextToken());  //나무높이
+		
+		int tmp = V-A; //낮밤 둘다 지내야 하는 최소 높이
+		
+		int cnt = 0;
+		if(tmp%(A-B)==0) {
+			cnt = tmp/(A-B) + 1;
+		}else {
+			cnt = tmp/(A-B) + 2;
+		}
+		
+		System.out.println(cnt);
+	}
 }
+
