3924d0aca0a677945fb01070c286280e5bdc5703
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-06-04 12:46:47 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1392 ms, Memory: 130072 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,55 +1,30 @@
-//문제번호: 2751
-//메모리:
-//시간:
-
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main{
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		ArrayList<Integer> list = new ArrayList();
+		for(int i=0;i<N;i++) {
+			int tmp = Integer.parseInt(br.readLine());
+			list.add(tmp);
+		}
+		
+		//Arrays.sort는 최악의 경우 O(n^2)이므로 에러남
+		//Collections.sort는 최악, 평균 모두 O(nlogn)으로 안정적
+		Collections.sort(list, (cur, pos)->{
+			return cur-pos;
+		});
 
-        int n = Integer.parseInt(br.readLine()); //수의 개수(1 ≤ N ≤ 1,000,000)
-        int[] arr = new int[n];
-        int max = Integer.MIN_VALUE;
-        int min = 0; //기본 최소갑을 0으로 설정. 즉 양수만 입력되는 경우 최소값은 자동으로 0!!
-
-        //배열값 입력
-        for(int i=0;i<n;i++) {
-            arr[i] = Integer.parseInt(br.readLine()); //절댓값이 1,000,000보다 작거나 같은 정수
-            if(max<arr[i]){
-                max = arr[i];
-            }
-            if(min>arr[i]){
-                min = arr[i];
-            }
-        }
-
-        //카운팅정렬
-        int[] cnt = new int[max-min+1]; //카운팅배열(0 ~ max+Math.abs(min)까지 저장)
-        for(int i=0;i<n;i++){
-            cnt[arr[i] + Math.abs(min)]++;
-        }
-        for(int i=1;i<cnt.length;i++){ //누적카운트 배열로 만들기
-            cnt[i] += cnt[i-1];
-        }
-
-        int[] sortArr = new int[n]; //정렬된 배열 만들기
-        for(int i=n-1;i>=0;i--){
-            int tmp = cnt[arr[i] + Math.abs(min)]-1;
-            sortArr[tmp] = arr[i] + Math.abs(min);
-            cnt[arr[i] + Math.abs(min)]--;
-        }
-
-        //결과 출력
-        for(int i=0;i<n;i++){
-            bw.write(sortArr[i]-Math.abs(min)+"\n"); //abs(min)만큼 반드시 빼줘야 함.
-        }
-
-        bw.flush();
-        bw.close();
-        br.close();
-
-    }
+		StringBuilder sb = new StringBuilder();
+		for(int e: list) {
+			sb.append(e).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+		
+	}
 }
