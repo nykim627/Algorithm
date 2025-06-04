86c661e33ceade2ebfc3ba65d403432c61672597
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-06-04 20:24:35 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1636 ms, Memory: 339344 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -9,14 +9,28 @@ public class Main{
 		int N = Integer.parseInt(br.readLine());
 		
 		int[] arr = new int[N];
+		int max = Integer.MIN_VALUE;
 		for(int i=0;i<N;i++) {
 			arr[i] = Integer.parseInt(br.readLine());
+			max = Math.max(max, arr[i]);
 		}
 		
-		Arrays.sort(arr);
+		int[] count = new int[max+1];
+		for(int i=0;i<N;i++) {
+			count[arr[i]]++;
+		}
+		
+		for(int i=1;i<max+1;i++) {
+			count[i] += count[i-1];
+		}
+		
+		int[] res = new int[N];
+		for(int i=N-1;i>=0;i--) {
+			res[--count[arr[i]]] = arr[i];
+		}
 		
 		StringBuilder sb = new StringBuilder();
-		for(int e: arr) {
+		for(int e: res) {
 			sb.append(e).append("\n");
 		}
 		
