6c0cd248496b8fdf10100e864698c7730a14c5ef
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-06-04 12:28:12 +0900

    [Silver V] Title: 단어 정렬, Time: 508 ms, Memory: 24232 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,41 +1,32 @@
-//문제번호: 1181
-//메모리:
-//시간:
-
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        //단어 정렬
-
-        int n = Integer.parseInt(br.readLine()); //단어 개수 (1 이상 20000 이하)
-        PriorityQueue<String> pq = new PriorityQueue<>((o1, o2) -> {
-            if (o1.length() == o2.length()) {
-                return o1.compareTo(o2);
-            } else {
-                return o1.length() - o2.length();
-            }
-        });
-
-        //배열값 입력
-        for (int i = 0; i < n; i++) {
-            String tmp = br.readLine();
-            if (!pq.contains(tmp)) {
-                pq.add(tmp);
-            }
-        }
-
-        int size = pq.size();
-        for (int i = 0; i < size; i++) {
-            bw.write(pq.poll() + "\n");
-        }
-
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+public class Main{
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		HashSet<String> set = new HashSet<String>();
+		for(int i=0;i<N;i++) {
+			String tmp = br.readLine();
+			set.add(tmp);
+		}
+		
+		String[] arr = new String[set.size()];
+		int idx = 0;
+		for(String e: set) {
+			arr[idx++] = e;
+		}
+		
+		Arrays.sort(arr, (cur,pos)->{
+			if(cur.length()==pos.length()) return cur.compareTo(pos);
+			return cur.length()-pos.length();
+		});
+		
+		for(String e: arr) {
+			System.out.println(e);
+		}
+		
+	}
 }
