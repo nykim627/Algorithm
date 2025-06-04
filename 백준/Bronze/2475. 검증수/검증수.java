d96bf8b7a2963dddaa4e73be7e9988a005d58da1
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-06-04 22:29:04 +0900

    [Bronze V] Title: 검증수, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -1,14 +1,22 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int sum=0;
-        for(int i=0;i<5;i++) {
-            int q = sc.nextInt();
-            sum += q * q;
-        }
-        System.out.println(sum%10);
-    }
+public class Main{
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[5];
+		
+		int sum = 0;
+		for(int i=0;i<5;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			sum += (int) Math.pow(arr[i], 2);
+		}
+		
+		System.out.println(sum%10);
+		
+	}
 }
