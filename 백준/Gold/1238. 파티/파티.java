bb6775b3c062519259aa5b263744c8ff256dd257
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-06-09 22:45:36 +0900

    [Gold III] Title: 파티, Time: 572 ms, Memory: 62864 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,16 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	static int N, M, X;
-	static int[][] resArr;
-	static int[][] arr;
 	static final int INF = Integer.MAX_VALUE;
+	static int[][] dist;
+	static ArrayList<Edge>[] arrList;
 
+	public static class Edge implements Comparable<Edge> {
+		int to;
+		int cost;
+		
+		public Edge(int to, int cost) {
+			this.to = to;
+			this.cost = cost;
+		}
+
+		@Override
+		public int compareTo(Edge o) {
+			return this.cost-o.cost;
+		}
+		
+		
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -20,58 +39,58 @@ public class Main {
 		M = Integer.parseInt(st.nextToken()); // 1 ≤ M ≤ 10,000
 		X = Integer.parseInt(st.nextToken()); // 1 ≤ X ≤ N
 
-		arr = new int[N + 1][N + 1];
-		for (int i = 1; i <= M; i++) {
+		arrList = new ArrayList[N+1];
+		for(int i=1;i<=N;i++) {
+			arrList[i] = new ArrayList<Edge>();
+		}
+		
+		for(int i=1;i<=M;i++) {
 			st = new StringTokenizer(br.readLine());
-			arr[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			arrList[from].add(new Edge(to, cost));
 		}
-
-		resArr = new int[N + 1][N + 1];
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				resArr[i][j] = INF;
-			}
+		
+		dist = new int[N+1][N+1];
+		for(int i=1;i<=N;i++) {
+			Arrays.fill(dist[i], INF);
 		}
-		for (int i = 1; i <= N; i++) {
+		for(int i=1;i<=N;i++) {
 			dijkstra(i);
 		}
-
+		
 		int max = Integer.MIN_VALUE;
-		for (int i = 1; i <= N; i++) {
-			max = Math.max(max, resArr[i][X] + resArr[X][i]);
+		for(int i=1;i<=N;i++) {
+			max = Math.max(max, dist[i][X]+dist[X][i]);
 		}
-
+		
 		System.out.println(max);
-//		System.out.println(sb.toString());
+
 	}
 
-	private static void dijkstra(int i) {
-		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
-			return o1[1] - o2[1];
-		});
-
-		boolean[] visited = new boolean[N + 1];
-
-		resArr[i][i] = 0;
-		pq.add(new int[] { i, 0 });
-
-		while (!pq.isEmpty()) {
-			int[] curr = pq.poll();
-
-			if (visited[curr[0]] == true) {
-				continue;
-			}
-			visited[curr[0]] = true;
-
-			for (int j = 1; j <= N; j++) {
-				if (!visited[j] && arr[curr[0]][j] > 0) {
-					if (resArr[i][j] > resArr[i][curr[0]] + arr[curr[0]][j]) {
-						resArr[i][j] = resArr[i][curr[0]] + arr[curr[0]][j];
-						pq.add(new int[] { j, resArr[i][j] });
-					}
+	private static void dijkstra(int start) {
+		PriorityQueue<Edge> pq = new PriorityQueue();
+		boolean[] visited = new boolean[N+1];
+		
+		dist[start][start] = 0;
+		pq.add(new Edge(start,0));
+		
+		while(!pq.isEmpty()) {
+			Edge curr = pq.poll();
+			
+			if(visited[curr.to]==true) continue;
+			visited[curr.to] = true;
+			
+			for(Edge e: arrList[curr.to]) {
+				if(!visited[e.to] && dist[start][e.to] > dist[start][curr.to]+ e.cost) {
+					dist[start][e.to] = dist[start][curr.to]+ e.cost;
+					pq.add(new Edge(e.to, dist[start][e.to]));
 				}
 			}
+			
 		}
-
+		
+		
 	}
 }
