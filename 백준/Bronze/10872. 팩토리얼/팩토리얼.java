bd0d1d5e5d57f1c5f9b440455f332bcfee802618
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-06-10 23:33:09 +0900

    [Bronze III] Title: 팩토리얼, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,14 +1,31 @@
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();
-        int mul = 1;
-        for(int i=n;i>=1;i--){
-            mul *= i;
-        }
-        
-        System.out.println(mul);
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		System.out.println(fact(N));
+	}
+	
+	static int fact(int n) {
+		if(n==0) {
+			return 1;
+		}
+		if(n==1) {
+			return 1;
+		}
+		return fact(n-1) * n;
+	}
+	
+
 }
