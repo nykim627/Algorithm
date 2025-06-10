39267a7b13536093d27de31c904b784b20df61ef
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-06-10 23:25:20 +0900

    [Bronze IV] Title: 녹색거탑, Time: 100 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static boolean[] visited;
+	static int num=0;
+	static int[] order;
+	static ArrayList<Integer>[] arrList;
+	static int V, E, R;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		
+		
+		
+		System.out.println((int)Math.pow(2, N));
+	}
+	
+
+}
