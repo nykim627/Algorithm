f6f55f5df2a2fcfe0fe9713a08d853f29d408124
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-06-12 14:29:09 +0900

    [Silver II] Title: 최소 힙, Time: 280 ms, Memory: 26208 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+	static int cnt;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		int T = Integer.parseInt(br.readLine());
+		for(int tc=1;tc<=T;tc++) {
+			int x = Integer.parseInt(br.readLine());
+			if(x==0) {
+				if(pq.isEmpty()) sb.append("0\n");
+				else sb.append(pq.poll()).append("\n");
+			}else {
+				pq.add(x);
+			}
+		}
+		System.out.println(sb.toString());
+		
+	}
+}
