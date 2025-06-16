d85d3fddaf0e9588dbf67b2ebc582522f85f120b
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-06-16 22:49:17 +0900

    [Bronze V] Title: 오늘 날짜, Time: 176 ms, Memory: 17192 KB -BaekjoonHub

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -1,22 +1,17 @@
-import java.time.LocalDate;
-import java.time.ZoneId;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args){
-        // 현재 날짜 구하기 (채점 기준 서버는 UTC+0)
-        LocalDate now = LocalDate.now(ZoneId.of("Asia/Seoul"));
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE));
+	}
 
-//        System.out.println(now); // 기본 포맷이 yyyy-mm-dd이므로 잘 나오지만 사용자 포맷 정의를 해보자
 
-        // 포맷 정의
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-        // 포맷 적용
-        String formattedNow = now.format(formatter);
-
-        // 결과 출력
-        System.out.println(formattedNow);
-
-    }
 }
