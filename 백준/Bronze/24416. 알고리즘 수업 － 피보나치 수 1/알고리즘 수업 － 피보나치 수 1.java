bfc2b76f4f5ab591d4004cbc74eea796e2dd7381
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-06-21 23:03:56 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 452 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -1,41 +1,43 @@
-//package Week3.nayoung;
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-//B24416 알고리즘 수업 - 피보나치 수 1
-//메모리
 public class Main {
-    static int cntFib = 0;
-    static int cntFibDP = 0;
-    static int[] memo;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        memo = new int[n+1];
-        fib(n); fibDP(n);
-
-        System.out.println(cntFib+" "+cntFibDP);
-
-        br.close();
-    }//main
-
-    static int fib(int n){
-        if(n==1 || n==2){
-            cntFib++;
-            return 1;
-        }
-        return fib(n-1) + fib(n-2);
-    }//fib(dp x)
-
-    static int fibDP(int n){
-        memo[1] = memo[2] = 1;
-        for(int i=3;i<=n;i++){
-            cntFibDP++;
-            memo[i] = memo[i-1]+memo[i-2];
-        }
-        return memo[n];
-    }//fibDP
+	static StringBuilder sb;
+	static int[] f;
+	static int cnt1=0, cnt2=0;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		f = new int[n+1];
+		
+		fib(n);
+		fibonacci(n);
+		
+		sb.append(cnt1).append(" ").append(cnt2);
+		System.out.println(sb.toString());
+	}
+	
+	//재귀
+	static int fib(int n) {
+		if(n==1 || n==2) {
+			cnt1++;
+			return 1;
+		}
+		return fib(n-1) + fib(n-2);
+	}
+	
+	//dp
+	static void fibonacci(int n) {
+		if(n <= 2) f[n] = 1;
+		else {
+			for(int i=3;i<=n;i++) {
+				f[i] = f[i-1] + f[i-2];
+				cnt2++;
+			}
+		}
+	}
+	
 }
