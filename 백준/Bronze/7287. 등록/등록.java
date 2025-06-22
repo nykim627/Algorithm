9d8893938e9c2b86c595e3e3489c26b872c299e5
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-06-22 22:57:34 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 13936 KB -BaekjoonHub

--- 백준/Bronze/7287. 등록/등록.java
+++ 백준/Bronze/7287. 등록/등록.java
@@ -1,6 +1,16 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("84");
-        System.out.println("ny627");
-    }
+	static StringBuilder sb;
+	static int[] f;
+	static int cnt1=0, cnt2=0;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		sb.append(166).append("\n").append("nykim627");
+		System.out.println(sb.toString());
+	}
+	
 }
