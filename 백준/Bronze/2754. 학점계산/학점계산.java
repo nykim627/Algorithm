d2091fc448bc3212d8244a23ffdce2b9f8230eba
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-06-29 23:05:24 +0900

    [Bronze III] Title: 학점계산, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/2754. 학점계산/학점계산.java
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -1,52 +1,61 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
-        String str = sc.next();
-        double grade = 0;
-        switch(str){
-            case "A+":
-                grade = 4.3;
-                break;
-            case "A0":
-                grade = 4.0;
-                break;
-            case "A-":
-                grade = 3.7;
-                break;
-            case "B+":
-                grade = 3.3;
-                break;
-            case "B0":
-                grade = 3.0;
-                break;
-            case "B-":
-                grade = 2.7;
-                break;
-            case "C+":
-                grade = 2.3;
-                break;
-            case "C0":
-                grade = 2.0;
-                break;
-            case "C-":
-                grade = 1.7;
-                break;
-            case "D+":
-                grade = 1.3;
-                break;
-            case "D0":
-                grade = 1.0;
-                break;
-            case "D-":
-                grade = 0.7;
-                break;
-            case "F":
-                grade = 0.0;
-                break;
-        }
-        System.out.println(grade);
-    }
+	static StringBuilder sb;
+	static int a, b, c;
+	static int[][][] dp;
+	static int INF = Integer.MAX_VALUE;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		
+		String order = br.readLine();
+		switch(order) {
+		case "A+":
+			sb.append("4.3");
+			break;
+		case "A0":
+			sb.append("4.0");
+			break;
+		case "A-":
+			sb.append("3.7");
+			break;
+		case "B+":
+			sb.append("3.3");
+			break;
+		case "B0":
+			sb.append("3.0");
+			break;
+		case "B-":
+			sb.append("2.7");
+			break;
+		case "C+":
+			sb.append("2.3");
+			break;
+		case "C0":
+			sb.append("2.0");
+			break;
+		case "C-":
+			sb.append("1.7");
+			break;
+		case "D+":
+			sb.append("1.3");
+			break;
+		case "D0":
+			sb.append("1.0");
+			break;
+		case "D-":
+			sb.append("0.7");
+			break;
+		case "F":
+			sb.append("0.0");
+			break;
+		}
+		
+		System.out.println(sb.toString());
+	}
 }
