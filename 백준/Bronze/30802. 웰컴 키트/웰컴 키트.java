959ede23088f2a2980e66457d8a27cc5bcfa6410
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-07-15 23:36:02 +0900

    [Bronze III] Title: 웰컴 키트, Time: 104 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -1,34 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int a = sc.nextInt();
-        int[] arr = new int[6];
-        for(int i=0;i<6;i++){
-            arr[i] = sc.nextInt();
-        }
-
-        int[] arr2 = new int[2];
-        for(int i=0;i<2;i++){
-            arr2[i] = sc.nextInt();
-        }
-
-        int m = 0;
-        for(int i=0;i<6;i++){
-            if(arr[i]%arr2[0]==0){
-                m+=arr[i]/arr2[0];
-            }else{
-                m+=(arr[i]/arr2[0]+1);
-            }
-        }
-
-        int n = a/arr2[1];
-        int k = a%arr2[1];
-
-        System.out.println(m);
-        System.out.printf("%d %d",n, k);
-
-
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] numberPerSize = new int[6];
+		for(int i=0;i<6;i++) {
+			numberPerSize[i] = Integer.parseInt(st.nextToken());
+		}
+		st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+		int P = Integer.parseInt(st.nextToken());
+		
+		int resT = 0;
+		for(int i=0;i<6;i++) {
+			resT += numberPerSize[i]/T;
+			if(numberPerSize[i]%T!=0) resT++;
+		}
+		
+		int resP1 = N/P;
+		int resP2 = N%P;
+		
+		sb.append(resT).append("\n").append(resP1).append(" ").append(resP2);
+		
+		System.out.println(sb.toString());
+	}
 }
