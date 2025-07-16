dcf50d6665e5aea81768e062800e4d75d13c1e78
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-07-16 23:22:17 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 104 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,37 +1,32 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int t = Integer.parseInt(br.readLine()); //tc수
-
-        for(int tc=1;tc<=t;tc++){
-            int k = Integer.parseInt(br.readLine()); //층
-            int n = Integer.parseInt(br.readLine()); //호
-
-            int[][] apt = new int[k+1][n+1]; //0~k층, 0~n호로 이루어진 아파트. (0호는 0명 살아서 상관 x)
-            for(int c=0;c<=n;c++){ //0층 채우기
-                apt[0][c] = c;
-            }
-            for(int r=1;r<=k;r++){ //1층부터 채우기
-                for(int c=1;c<=n;c++){ //1호부터 채우기
-                    apt[r][c] = apt[r][c-1] + apt[r-1][c]; //k층 n호 인원 = k층 n-1호 인원 + k-1층 n호 인원
-                }
-            }
-//            System.out.println(Arrays.deepToString(apt));
-
-            bw.write(apt[k][n]+""); //bw.write로 숫자 출력 시 문자열로 변환후 출력시켜줘야 한다!!!
-            bw.newLine();
-
-
-        }//tc순회끝
-
-        bw.flush();
-        bw.close();
-        br.close();
-
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		for(int tc=0;tc<T;tc++) {
+			int k = Integer.parseInt(br.readLine());
+			int n = Integer.parseInt(br.readLine());
+			
+			int[][] arr = new int[k+1][n+1];
+			for(int j=1;j<=n;j++) {
+				arr[0][j] = j;
+			}
+			for(int i=1;i<=k;i++) {
+				for(int j=1;j<=n;j++) {
+					arr[i][j] = arr[i-1][j] + arr[i][j-1];
+				}
+			}
+			sb.append(arr[k][n]).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
 }
