166923b1e6bc7e841bad8b0d9b23b163bad3705d
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-08-03 22:06:56 +0900

    [Gold II] Title: 미확인 도착지, Time: 1396 ms, Memory: 162892 KB -BaekjoonHub

---
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.java
@@ -0,0 +1,111 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Main {
+	static int n, g, h;
+	static int INF = Integer.MAX_VALUE;
+	static ArrayList<Edge>[] edges;
+	
+	static class Edge implements Comparable<Edge>{
+		int to;
+		int cost;
+		boolean passedG, passedH; //해당 경로마다 g, h 지나는지 확인
+		public Edge(int to, int cost, boolean g, boolean h) {
+			this.to = to;
+			this.cost = cost;
+			this.passedG = g;
+			this.passedH = h;
+		}
+		@Override
+		public int compareTo(Edge o) {
+			return this.cost - o.cost;
+		}
+	}
+	
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringBuilder sb = new StringBuilder();
+    	
+    	int T = Integer.parseInt(br.readLine());
+    	for(int tc=0;tc<T;tc++) {
+    		StringTokenizer st = new StringTokenizer(br.readLine());
+    		n = Integer.parseInt(st.nextToken()); //교차로 수
+    		int m = Integer.parseInt(st.nextToken()); //도로 수
+    		int t = Integer.parseInt(st.nextToken()); //목적지 후보 수
+    		st = new StringTokenizer(br.readLine());
+    		int s = Integer.parseInt(st.nextToken()); //출발지
+    		g = Integer.parseInt(st.nextToken()); //g에서 h사이 반드시 지남
+    		h = Integer.parseInt(st.nextToken()); 
+    		
+    		edges = new ArrayList[n+1];
+    		for(int i=1;i<=n;i++) {
+    			edges[i] = new ArrayList();
+    		}
+    		for(int i=0;i<m;i++) {
+    			st = new StringTokenizer(br.readLine());
+    			int a = Integer.parseInt(st.nextToken());
+    			int b = Integer.parseInt(st.nextToken());
+    			int d = Integer.parseInt(st.nextToken());
+    			edges[a].add(new Edge(b,d, false, false));
+    			edges[b].add(new Edge(a,d, false, false)); //양방향
+    		}
+    		
+    		int[] ends = new int[t];
+    		for(int i=0;i<t;i++) {
+    			ends[i] = Integer.parseInt(br.readLine());
+    		}
+    		Arrays.sort(ends); //목적지 후보 배열 오름차순 정렬
+    		
+    		for(int i=0;i<t;i++) {
+    			int total = dijkstra(s,ends[i]);
+    			int SG = dijkstra(s, g);
+    			int GH = dijkstra(g, h);
+    			int HE = dijkstra(h, ends[i]);
+    			int SH = dijkstra(s, h);
+    			int GE = dijkstra(g, ends[i]);
+    			if((total==SG+GH+HE)||(total==SH+GH+GE)){
+    				sb.append(ends[i]).append(" ");
+    			}
+    		}
+    		
+    		sb.append("\n");
+    		
+    	}//tc
+    	
+    	System.out.println(sb.toString());
+    }
+
+	private static int dijkstra(int start, int end) {
+		PriorityQueue<Edge> pq = new PriorityQueue(); //간선 저장 pq
+		pq.add(new Edge(start,0, false, false));
+		int[] dist = new int[n+1];
+		Arrays.fill(dist, INF);
+		dist[start] = 0;
+		
+		while(!pq.isEmpty()) {
+			Edge curr = pq.poll();
+			if(curr.to==end) {
+				return dist[end];
+			}
+			for(Edge e: edges[curr.to]) {
+				if(dist[e.to] > dist[curr.to]+e.cost) {
+					dist[e.to] = dist[curr.to]+e.cost; //최소값 갱신
+					boolean passG=curr.passedG, passH=curr.passedH;
+					if(e.to==g) {
+						passG = true;
+					}else if(e.to==h) {
+						passH = true;
+					}
+					pq.add(new Edge(e.to, dist[e.to], passG, passH));
+				}
+			}
+		}
+		
+		return -1;
+	}
+}
