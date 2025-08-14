390bcf4e4b0ffe033873cdb8d1347d19d6c629d1
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-08-14 19:10:31 +0900

    [Bronze V] Title: 홍익대학교, Time: 172 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        System.out.println(n-1946);
+    }
+}
