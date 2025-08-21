3d7ec2a0f3a01d2e1c7e6b5d9764f5d1447eff14
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-08-21 09:27:55 +0900

    [Bronze V] Title: Rats, Time: 104 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/18301. Rats/Rats.java
@@ -0,0 +1,16 @@
+//N := ⌊(n1 + 1)(n2 + 1)/(n12 + 1) - 1⌋
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+        int n12 = Integer.parseInt(st.nextToken());
+        int res = (n1 + 1)*(n2 + 1)/(n12 + 1) - 1;
+        System.out.println(res);
+    }
+}
