612cc70b46b6903f279d327a274ed15d1b4d55c2
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-08-30 12:39:05 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 524 ms, Memory: 143064 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,11 +2,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
+
+//유니온파인드 이용
 class Main {
+	static class UnionFind{
+		int[] parent, rank;
+		UnionFind(int n){
+			parent = new int[n+1];
+			rank = new int[n+1];
+			for(int i=1;i<=n;i++) parent[i] = i; //makeSet
+		}
+		
+		int findSet(int x) {
+			if(parent[x]!=x) parent[x] = findSet(parent[x]);
+			return parent[x];
+		}
+		
+		void union(int x, int y) {
+			x = findSet(x); y = findSet(y);
+			if(x==y) return; //대표자가 같으면 사이클이 있는것. 트리는 사이클 있으면 안됨
+			if(rank[x]<rank[y]) parent[x]=y; //y랭크가 더 높으면 y가 대표자가 되도록 합쳐
+			else {
+				parent[y]=x;
+				if(rank[x]==rank[y]) rank[x]++; //두 랭크가 같으면 x 랭크 하나 높여
+			}
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,31 +41,24 @@ class Main {
 		for(int i=1;i<=V;i++) {
 			arrList[i] = new ArrayList<Integer>();
 		}
+
+		UnionFind uf = new UnionFind(V); //정점 개수 V만큼의 부모배열, 랭크배열 만들기
 		for(int i=0;i<E;i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			arrList[a].add(b);
 			arrList[b].add(a); //양방향
+			uf.union(a,b); //a가 속한 집합과 b가 속한 집합을 합치자 (findSet은 이 함수 안에서 함)
 		}
 		
-		boolean[] visited = new boolean[V+1];
-		int cnt = 0;
+		//대표만 확인하자
+		HashSet<Integer> set = new HashSet<>();
 		for(int i=1;i<=V;i++) {
-			if(visited[i]) continue;
-			dfs(arrList,i,visited);
-			cnt++;
+			set.add(uf.findSet(i));
 		}
 		
-		System.out.println(cnt);
+		System.out.println(set.size());
 		
-	}
-
-	private static void dfs(ArrayList<Integer>[] arrList, int i, boolean[] visited) {
-		for(int e : arrList[i]) {
-			if(visited[e]) continue;
-			visited[e] = true;
-			dfs(arrList, e, visited);
-		}
 	}
 }
