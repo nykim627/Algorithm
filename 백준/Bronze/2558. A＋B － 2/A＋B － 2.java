4dee6dee3fd6331c341483e90610fba6e4d1ae53
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-09-02 23:31:10 +0900

    [Bronze V] Title: A+B - 2, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -1,23 +1,18 @@
-import java.io.*;
-import java.math.BigInteger;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
+class Main {
 
-public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		System.out.println(A+B);
+	}
 
-    public static void main(String[] args) throws IOException {
-        //1 ≤ m ≤ n ≤ 10^1000
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int a = Integer.parseInt(br.readLine());
-        int b = Integer.parseInt(br.readLine());
-
-
-        bw.write(a+b+"");
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
 }
