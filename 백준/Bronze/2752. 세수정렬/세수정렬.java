bee7c67eb5b330dd49e46c7983b05bdbad86fc23
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-09-05 22:50:33 +0900

    [Bronze IV] Title: 세수정렬, Time: 120 ms, Memory: 15940 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[3];
+		arr[0] = Integer.parseInt(st.nextToken());
+		arr[1] = Integer.parseInt(st.nextToken());
+		arr[2] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(arr);
+		for(int e: arr) {
+			System.out.print(e+" ");
+		}
+	}
+
+}
