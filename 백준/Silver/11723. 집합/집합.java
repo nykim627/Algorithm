98dbb1e4156836b437c7e46aea67fe3537e7743e
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-09-08 21:14:39 +0900

    [Silver V] Title: 집합, Time: 1208 ms, Memory: 308380 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,50 +1,47 @@
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
 
-//B11723 근손실
-//
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+class Main {
 
-        int M = Integer.parseInt(br.readLine());
-        int S = 0;
-
-        for(int i=0;i<M;i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String order = st.nextToken();
-            switch (order){
-                case "add":
-                    int x1 = Integer.parseInt(st.nextToken());
-                    if((S&(1<<x1))==0) S = S | (1<<x1);
-                    break;
-                case "remove":
-                    int x2 = Integer.parseInt(st.nextToken());
-                    if((S&(1<<x2))!=0) S = S & ~(1<<x2);
-                    break;
-                case "check":
-                    int x3 = Integer.parseInt(st.nextToken());
-                    if((S&(1<<x3))!=0) bw.write(1+"\n");
-                    else if((S&(1<<x3))==0) bw.write(0+"\n");
-                    break;
-                case "toggle":
-                    int x4 = Integer.parseInt(st.nextToken());
-                    if((S&(1<<x4))!=0) S = S & ~(1<<x4);
-                    else if((S&(1<<x4))==0) S = S | (1<<x4);
-                    break;
-                case "all":
-                    S = ~S^S;
-                    break;
-                case "empty":
-                    S = 0;
-                    break;
-            }
-        }
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }//main
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int M = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		HashSet<Integer> set = new HashSet<>();
+		for(int i=0;i<M;i++) {
+			String[] orders = br.readLine().split(" ");
+			switch(orders[0]) {
+			case "add":
+				int num1 = Integer.parseInt(orders[1]);
+				if(!set.contains(num1)) set.add(num1);
+				break;
+			case "remove":
+				int num2 = Integer.parseInt(orders[1]);
+				if(set.contains(num2)) set.remove(num2);
+				break;
+			case "check":
+				int num3 = Integer.parseInt(orders[1]);
+				if(set.contains(num3)) sb.append(1).append("\n");
+				else sb.append(0).append("\n");
+				break;
+			case "toggle":
+				int num4 = Integer.parseInt(orders[1]);
+				if(set.contains(num4)) set.remove(num4);
+				else set.add(num4);
+				break;
+			case "all":
+				for(int num5=1;num5<=20;num5++) {
+					set.add(num5);
+				}
+				break;
+			case "empty":
+				set.clear();
+				break;
+			}
+		}
+		System.out.println(sb.toString());
+	}
 
 }
