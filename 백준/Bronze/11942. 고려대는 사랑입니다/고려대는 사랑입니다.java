67c5c9dfd8e9044370c95de936bbd712a2b50554
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-09-17 16:06:32 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 92 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+
+class Main {
+	static int[] nums = {1,2,3};
+	public static void main(String[] args) throws IOException {
+		System.out.println("고려대학교");
+		
+	}
+
+}
