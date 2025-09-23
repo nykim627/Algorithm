92271965359de94971cc4d2cec8f7810c6711340
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-09-24 08:44:02 +0900

    [Bronze V] Title: 콜센터, Time: 96 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
