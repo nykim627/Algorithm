6181747f22870a09684cc46751efd49fbb174bca
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-09-26 00:07:39 +0900

    [Bronze II] Title: OX퀴즈, Time: 108 ms, Memory: 14896 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -1,32 +1,22 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        //OX퀴즈
-
-        int t = sc.nextInt(); //tc 개수
-
-        for(int tc=0;tc<t;tc++){
-            String str = sc.next(); //퀴즈 결과 문자열
-
-            int sum = 0; // 총점수
-            int subsum = 0; //부분합(연속 O 점수)
-            for(int i=0;i<str.length();i++){
-                if(str.charAt(i)=='O'){
-                    subsum++;
-                }else{
-                    subsum = 0; // X 만나면 부분합 초기화
-                }
-                sum += subsum;
-            }//for문 끝
-            System.out.println(sum);
-        }//tc순회 끝
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int tc=0;tc<T;tc++) {
+			String[] str = br.readLine().split("X");
+			int point = 0;
+			for(String e: str) {
+				int N = e.length();
+				point += N*(N+1)/2;
+			}
+			System.out.println(point);
+		}
+	}
 
-        sc.close();
-    }//main끝
 }
