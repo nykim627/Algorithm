62e6ea1e973c352775ea2e16542d8d7eb502a92f
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-09-26 22:52:34 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14200 KB, Score: 50 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,34 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int l = sc.nextInt();
-        String str = sc.next();
-        long sum = 0;
-        long m = 1234567891;
-        int r = 31;
-        
-        for (int i = 0; i < l; i++) {
-            long value = (str.charAt(i) - 'a' + 1);
-            sum = (sum + (value * modPow(r, i, m)) % m) % m;  // 모듈러 연산 적용
-        }
 
-        System.out.println(sum);
-        sc.close();
-    }
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int L = Integer.parseInt(br.readLine());
+		String str = br.readLine();
+		long sum = 0;
+		for(int i=0;i<L;i++) {
+			long num = str.charAt(i)-'a'+1;
+			sum += num*Math.pow(31, i);
+		}
+		System.out.println(sum%1234567891);
+	}
 
-    // 빠른 거듭제곱 (Exponentiation by Squaring)
-    public static long modPow(long base, int exp, long mod) {
-        long result = 1;
-        while (exp > 0) {
-            if (exp % 2 == 1) {  // 홀수이면 base를 한 번 곱함
-                result = (result * base) % mod;
-            }
-            base = (base * base) % mod;  // base를 제곱하여 지수를 줄임
-            exp /= 2;
-        }
-        return result;
-    }
 }
