af195bab7347324539e33fdb0c0f8b612c58c60f
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-10-02 23:57:28 +0900

    [Bronze IV] Title: 관공... 어찌하여 목만 오셨소..., Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/30501. 관공．．． 어찌하여 목만 오셨소．．．/관공．．． 어찌하여 목만 오셨소．．．.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for(int i=0;i<N;i++) {
+			String str = br.readLine();
+			if(str.contains("S")) {
+				System.out.println(str);
+				break;
+			}
+		}
+		
+	}
+}
