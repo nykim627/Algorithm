76c1467cf3c156bd282b337dd0f1f4577f584206
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-10-03 21:09:35 +0900

    [Bronze I] Title: FizzBuzz, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
@@ -1,49 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
 
-    public static void main(String[] args) throws NumberFormatException{
-        Scanner sc = new Scanner(System.in);
-        String a1 = sc.next();
-        if(a1.equals("FizzBuzz")){
-            String a2 = sc.next();
-            String a3 = sc.next();
-            System.out.println("Fizz");
-        }else if(a1.equals("Fizz")){
-            String a2 = sc.next();
-            String a3 = sc.next();
-            if(a2.equals("Buzz")){
-                System.out.println("Fizz");
-            }else {
-                int a4 = Integer.parseInt(a2)+2;
-                if(a4%5==0){
-                    System.out.println("FizzBuzz");
-                }else{
-                    System.out.println("Fizz");
-                }
-            }
-        }else if(a1.equals("Buzz")){
-            String a2 = sc.next();
-            String a3 = sc.next();
-            if(a2.equals("Fizz")){
-                System.out.println(Integer.parseInt(a3) + 1);
-            }else {
-                int a4 = Integer.parseInt(a2)+2;
-                if(a4%3==0){
-                    System.out.println("Fizz");
-                }else{
-                    System.out.println(a4);
-                }
-            }
-        }else{
-            String a2 = sc.next();
-            String a3 = sc.next();
-            int a4 = Integer.parseInt(a1)+3;
-            if(a4%5==0){
-                System.out.println("Buzz");
-            }else{
-                System.out.println(a4);
-            }
-        }
-    }
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = 0;
+		for(int i=0;i<3;i++) {
+			String str = br.readLine();
+			if(!(str.equals("FizzBuzz")||str.equals("Fizz")||str.equals("Buzz"))) {
+				num = Integer.parseInt(str)+(3-i);
+				break;
+			}
+		}
+		if(num%15==0) {
+			System.out.println("FizzBuzz");
+		}else if(num%3==0) {
+			System.out.println("Fizz");
+		}else if(num%5==0) {
+			System.out.println("Buzz");
+		}else {
+			System.out.println(num);
+		}
+	}
 }
