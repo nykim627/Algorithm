6c3efd8c2ab8cda09424dad72176257a36845775
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-10-09 13:27:48 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -13,12 +13,13 @@ class Main{
             P[i] = Integer.parseInt(st.nextToken());
         }
         
-        int[] dp = new int[N+2];
+        //dp[i] = i일부터 N일까지 얻을 수 있는 최대 수익
+        int[] dp = new int[N+2]; //N+1일은 최대 수익 0.
         for(int i=N;i>0;i--){
-            if(i+T[i] > N+1) dp[i] = dp[i+1];
+            if(i+T[i] > N+1) dp[i] = dp[i+1]; //얘 때문에 dp범위 N+2로 설정. 최대 N일에 1일동안 상담 = N+1이므로 이거 초과하면 안되는것.
             else{
-                int nonSelect = dp[i+1];
-                int select = P[i] + dp[i+T[i]];
+                int nonSelect = dp[i+1]; //이날 상담 선택 안하면 그전 dp값
+                int select = P[i] + dp[i+T[i]]; //선택하면 i+T[i]일부터 N일까지의 보상 + 이날 보상
                 dp[i] = Math.max(nonSelect, select);
             }
         }
