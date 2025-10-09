fb8304d4c38f80a173841712fc0f9a10fbc554fb
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-10-09 12:58:22 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,37 +1,30 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-
-		int N = Integer.parseInt(br.readLine());
-		Stack<Integer> stack;
-		for(int i=0;i<N;i++) {
-			String tmp = br.readLine();
-			stack = new Stack();
-			boolean isVPS = true;
-			for(int j=0;j<tmp.length();j++) {
-				if(tmp.charAt(j)=='(') {
-					stack.add(1);
-				}else {
-					if(stack.isEmpty()) {
-						isVPS = false;
-					}else {
-						stack.pop();
-					}
-				}
-			}
-			if(stack.size()==0 && isVPS) {
-				sb.append("YES").append("\n");
-			}else {
-				sb.append("NO").append("\n");
-			}
-		}
-		
-		System.out.println(sb.toString());
-
-	}
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<N;i++){
+            String str = br.readLine();
+            Stack<Character> stack = new Stack<>();
+            boolean flag = true;
+            for(int j=0;j<str.length();j++){
+                char tmp = str.charAt(j);
+                if(tmp=='(') stack.push(tmp);
+                else {
+                    if(stack.isEmpty()) {
+                        flag = false;
+                        break;
+                    } else{
+                        stack.pop();
+                    }
+                }
+            }
+            if(flag && stack.isEmpty()) sb.append("YES").append("\n");
+            else sb.append("NO").append("\n");
+        }
+        System.out.println(sb.toString());
+    }
 }
