294f8bddd16d6152f39d4cd535db003e5c293dd3
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-10-10 20:54:10 +0900

    [Silver I] Title: 단지번호붙이기, Time: 104 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,62 +1,63 @@
 import java.util.*;
 import java.io.*;
 
-public class Main
-{
-    static boolean[][] visited;
+class Main{
+    static boolean[][] map;
+    static int N, num; //단지번호
     static int[] dr = {-1,1,0,0};
     static int[] dc = {0,0,-1,1};
-    static int N;
-    static int[][] map;
-    static int cnt; //총 단지수
-    static int subcnt; //단지 당 세대수
-    
-    public static void main(String[] args) throws IOException {
-        StringBuilder sb = new StringBuilder();
+    static int[][] visited;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+        StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
+        map = new boolean[N][N];
         for(int i=0;i<N;i++){
-            String row = br.readLine();
+            String str = br.readLine();
             for(int j=0;j<N;j++){
-                int tmp = row.charAt(j) - '0';
-                map[i][j] = tmp;
-            }
-        }
-
-        ArrayList<Integer> arrList = new ArrayList();
-        visited = new boolean[N][N];
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(map[i][j]==0 || visited[i][j]) continue;
-                dfs(i,j);
-                cnt++; 
-                arrList.add(subcnt);
-                subcnt = 0; //단지 당 세대수 초기화
+                map[i][j] = str.charAt(j)=='0' ? false : true;
             }
         }
         
-        Collections.sort(arrList);
-        
-        System.out.println(cnt);
-        for(int e: arrList) {
-        	System.out.println(e);
+        num = 1;
+        visited = new int[N][N];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                if(map[i][j]&&visited[i][j]==0) bfs(i,j,num++);
+            }
         }
         
+        num--; //마지막에 하나 더 올라가서 낮춰줌
+        int[] cnt = new int[num+1]; //각 단지번호당 세대수
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                if(visited[i][j]>0) cnt[visited[i][j]]++;
+            }
+        }
+        Arrays.sort(cnt);
+        sb.append(num).append("\n");
+        for(int i=1;i<=num;i++){
+            sb.append(cnt[i]).append("\n");
+        }
+        System.out.println(sb.toString());
     }
-
-	private static void dfs(int i, int j) {
-		visited[i][j] = true;
-		subcnt++;
-		
-		for(int d=0;d<4;d++) {
-			int nr = i + dr[d];
-			int nc = j + dc[d];
-			if(nr<0 || nc<0 || nr>=N || nc>=N) continue;
-			if(visited[nr][nc]) continue;
-			if(map[nr][nc]==0) continue;
-			dfs(nr,nc);
-		}
-	}
+    
+    public static void bfs(int r, int c, int num){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{r,c});
+        visited[r][c] = num;
+        
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            for(int d=0;d<4;d++){
+                int nr = curr[0]+dr[d];
+                int nc = curr[1]+dc[d];
+                if(nr<0||nc<0||nr>=N||nc>=N) continue;
+                if(!map[nr][nc]) continue;
+                if(visited[nr][nc]!=0) continue;
+                visited[nr][nc] = num;
+                q.add(new int[]{nr,nc});
+            }
+        }
+    }
 }
