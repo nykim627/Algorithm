74b000a95e0784ce7cfb8bea6be47ab42b04a993
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-10-10 20:18:10 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 104 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,41 +1,14 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		String input = br.readLine();
-		if(input.contains("c=")) {
-			input = input.replace("c=", "*");
-		}
-		if(input.contains("c-")) {
-			input = input.replace("c-", "*");
-		}
-		if(input.contains("dz=")) {
-			input = input.replace("dz=","*");
-		}
-		if(input.contains("d-")) {
-			input = input.replace("d-", "*");
-		}
-		if(input.contains("lj")) {
-			input = input.replace("lj", "*");
-		}
-		if(input.contains("nj")) {
-			input = input.replace("nj", "*");
-		}
-		if(input.contains("s=")) {
-			input = input.replace("s=", "*");
-		}
-		if(input.contains("z=")) {
-			input = input.replace("z=", "*");
-		}
-		
-		System.out.println(input.length());
-		
-		br.close();
-
-	}
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String[] arr = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+        for(String e: arr){
+            str = str.replace(e, "*");
+        }
+        System.out.println(str.length());
+    }
 }
