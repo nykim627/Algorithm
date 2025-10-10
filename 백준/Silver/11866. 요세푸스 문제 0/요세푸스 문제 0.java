1f837147896ec698daa3b84273ac22eba31742b5
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-10-10 20:31:15 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 144 ms, Memory: 23100 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,35 +1,33 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		Queue<Integer> q = new LinkedList();
-		
-		for(int i=1;i<=N;i++) {
-			q.add(i);
-		}
-		
-		sb.append("<");
-		while(!q.isEmpty()) {
-			for(int i=0;i<K-1;i++) {
-				q.add(q.poll());
-			}
-			sb.append(q.poll());
-			if(q.isEmpty()) {
-				sb.append(">");
-			}else {
-				sb.append(", ");
-			}
-		}
-		
-		System.out.println(sb.toString());
-
-	}
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=1;i<=N;i++){
+            q.add(i);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        while(!q.isEmpty()){
+            //k-1번 회전
+            for(int j=1;j<K;j++){
+                q.add(q.poll());
+            }
+            //k번째 사람 제거
+            int dead = q.poll();
+            sb.append(dead);
+            if(q.isEmpty()){
+                sb.append(">");
+            }else{
+                sb.append(", ");
+            }
+        }
+        System.out.println(sb.toString());
+    }
 }
