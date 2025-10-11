2805c4bb8b115600292b2c9cda3e78ad291593e2
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-10-11 21:54:44 +0900

    [Silver V] Title: 덩치, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,27 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-
-class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
-		int[][] arr = new int[N][2];
-		for(int i=0;i<N;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			arr[i][0] = Integer.parseInt(st.nextToken());
-			arr[i][1] = Integer.parseInt(st.nextToken());
-		}
-		for(int i=0;i<N;i++) {
-			int cnt = 1;
-			for(int j=0;j<N;j++) {
-				if(i!=j&&arr[i][0]<arr[j][0]&&arr[i][1]<arr[j][1]) cnt++;
-			}
-			sb.append(cnt).append(" ");
-		}
-		System.out.println(sb.toString());
-	}
+class Main{
+    static class Person{
+        int w, h;
+        Person(int w, int h){
+            this.w = w;
+            this.h = h;
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Person[] arr = new Person[N];
+        for(int i=0;i<N;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int h = Integer.parseInt(st.nextToken());
+            arr[i] = new Person(w,h);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<N;i++){ //me
+            int rank = 1;
+            for(int j=0;j<N;j++){ //you
+                if(i==j) continue;
+                if(arr[i].w < arr[j].w && arr[i].h < arr[j].h) rank++;
+            }
+            sb.append(rank).append(" ");
+        }
+        
+        System.out.println(sb.toString());
+    }
+    
 }
