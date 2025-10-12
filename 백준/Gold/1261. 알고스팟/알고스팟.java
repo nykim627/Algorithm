6ebf134c0b46f596dc70fdc67850bdac1c4faf13
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-10-12 10:03:41 +0900

    [Gold IV] Title: 알고스팟, Time: 112 ms, Memory: 14524 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,69 +1,67 @@
 import java.util.*;
 import java.io.*;
 
+
+//0-1 BFS
 class Main{
-	static boolean[][] map;
+	static int[][] map;
 	static int[] dr = {-1,1,0,0};
 	static int[] dc = {0,0,-1,1};
 	static int N, M;
-	static boolean[][][] visited; //[r][c][time] <- 벽 부순 횟수
+	static int[][] dist; //벽 부순 횟수 담는 배열
+	static int INF = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        map = new boolean[N][M];
+        map = new int[N][M];
+        
+        int cntOnes=0;
         for(int i=0;i<N;i++) {
         	String str = br.readLine();
         	for(int j=0;j<M;j++) {
-        		map[i][j] = str.charAt(j)=='1' ? false : true;
+        		map[i][j] = str.charAt(j)-'0'; //빈방 0, 벽은 1
         	}
         }
-        visited = new boolean[N][M][N*M+10];
-        bfs(0,0);
+        int res = bfs(0,0);
         
-        int minTime = N*M;
-        for(int i=0;i<N*M+10;i++) {
-        	if(visited[N-1][M-1][i]) {
-        		minTime = i;
-        		break;
-        	}
-        }
-        System.out.println(minTime);
+        System.out.println(res);
     }
-    static class Pos implements Comparable<Pos>{
-    	int r, c, time;
-    	Pos(int r, int c, int time){
+    static class Pos{
+    	int r, c;
+    	Pos(int r, int c){
     		this.r = r;
     		this.c = c;
-    		this.time = time;
     	}
-		@Override
-		public int compareTo(Main.Pos o) {
-			return this.time-o.time;
-		}
     	
     }
-	private static void bfs(int i, int j) {
-		PriorityQueue<Pos> pq = new PriorityQueue<>();
-		pq.add(new Pos(i,j,0));
-		visited[i][j][0] = true;
+	private static int bfs(int i, int j) {
+		Deque<Pos> dq = new ArrayDeque<>();  //0-1 bfs는 덱 사용.
 		
-		while(!pq.isEmpty()) {
-			Pos curr = pq.poll();
-			if(curr.r==N-1&&curr.c==M-1) return;
+		//빈칸(비용 0)은 addFirst, 벽(비용 1)은 addLast
+		dq.addFirst(new Pos(i,j));
+		dist = new int[N][M];
+		for(int k=0;k<N;k++) {
+			Arrays.fill(dist[k], INF); //dist 배열값 INF로 초기화
+		}
+		dist[i][j] = 0;
+		
+		while(!dq.isEmpty()) {
+			Pos curr = dq.poll();
+			if(curr.r==N-1&&curr.c==M-1) return dist[N-1][M-1];
 			for(int d=0;d<4;d++) {
 				int nr = curr.r+dr[d];
 				int nc = curr.c+dc[d];
 				if(nr<0||nc<0||nr>=N||nc>=M) continue;
-				if(!map[nr][nc]&&!visited[nr][nc][curr.time+1]) {
-					visited[nr][nc][curr.time+1]=true;
-					pq.add(new Pos(nr,nc,curr.time+1));
-				}else if(map[nr][nc]&&!visited[nr][nc][curr.time]){
-					visited[nr][nc][curr.time]= true;
-					pq.add(new Pos(nr,nc,curr.time));
+				int w = map[nr][nc]; //map[nr][nc] : 빈칸=0, 벽=1 -> 즉 벽이면 부순횟수 +1
+				if(dist[nr][nc] > dist[curr.r][curr.c] + w) {  
+					dist[nr][nc] = dist[curr.r][curr.c] + w;
+					if(w==0) dq.addFirst(new Pos(nr,nc));
+					else dq.addLast(new Pos(nr,nc));
 				}
 			}
 		}
+		return -1;
 	}
 }
