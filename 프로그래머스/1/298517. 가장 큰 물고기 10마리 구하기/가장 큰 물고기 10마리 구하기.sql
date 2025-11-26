2f2257a1802ca2d9f80715176df61b1c82d78f21
imnyoung <113084198+nykim627@users.noreply.github.com>
2025-11-26 09:11:53 +0900

    [level 1] Title: 가장 큰 물고기 10마리 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
+++ 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
@@ -1,5 +1,5 @@
 -- 코드를 작성해주세요
 SELECT id, length
 FROM fish_info
-ORDER BY length DESC
+ORDER BY length DESC, id  -- null이 있는 컬럼의 정렬은 어떻게?? (https://thinkcatlog.tistory.com/entry/SQL-ORDER-BY%EB%A5%BC-%ED%96%88%EC%9D%84%EB%95%8C-NULL%EC%9D%B4-%EC%9E%88%EC%9C%BC%EB%A9%B4-%EC%96%B4%EB%96%BB%EA%B2%8C-%EC%A0%95%EB%A0%AC%EB%90%A0%EA%B9%8C)
 LIMIT 10
