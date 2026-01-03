b2e754170c318d2dd53ba9d2daa17ab97f1a21b8
imnyoung <113084198+nykim627@users.noreply.github.com>
2026-01-03 11:25:38 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,8 +1,6 @@
--- 코드를 입력하세요
-SELECT board.title title, board.board_id board_id, reply.reply_id reply_id, reply.writer_id writer_id, reply.contents contents, DATE_FORMAT(reply.created_date, '%Y-%m-%d') created_date
-FROM used_goods_board board
-INNER JOIN used_goods_reply reply
-ON board.board_id = reply.board_id
-WHERE YEAR(board.created_date) = 2022
-AND MONTH(board.created_date) = 10
-ORDER BY reply.created_date, board.title
+SELECT title, T1.board_id, reply_id, T2.writer_id, T2.contents, DATE_FORMAT(T2.created_date, '%Y-%m-%d') created_date
+FROM used_goods_board T1
+JOIN used_goods_reply T2
+ON T1.board_id = T2.board_id
+WHERE YEAR(T1.created_date)=2022 AND MONTH(T1.created_date)=10
+ORDER BY T2.created_date, title
